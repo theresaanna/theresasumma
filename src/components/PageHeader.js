7639697f35126d3b3496ce033d591abde85aacd8
Theresa Summa <theresasumma@gmail.com>
2022-02-20 22:40:44 -0800

    adding large header star

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 
 import "./PageHeader.sass"
+import DarkPurpleStar from "../images/dark-purple-star.svg"
 
 const PageHeader = () => {
   return (
@@ -8,6 +9,7 @@ const PageHeader = () => {
       <div className="headers">
         <h1>Theresa Summa</h1>
         <h2>User Interface Engineer</h2>
+        <img src={DarkPurpleStar} className="large-star"/>
       </div>
     </header>
   )
