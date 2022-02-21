7ea91bae12fc0878f84a609392fc23c79be206e4
Theresa Summa <theresasumma@gmail.com>
2022-02-20 17:36:21 -0800

    adding initial page header

---
+++ src/components/PageHeader.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+const PageHeader = () => {
+  return (
+    <header className="main">
+      <h1>Theresa Summa: Front End Developer</h1>
+    </header>
+  )
+}
+
+export default PageHeader
